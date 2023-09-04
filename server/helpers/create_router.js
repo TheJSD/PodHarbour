49b0fb605384eea02812da6830074a7248c59bc3
Lewis Ferguson <lewis.h.ferguson@btinternet.com>
2023-09-04 18:51:55 +0100

    added some more routes to creater_router.js

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -9,13 +9,34 @@ const createRouter = function (collection) {
         collection
         .find()
         .toArray()
-        .then((docs) => docs.json())
+        .then((docs) => res.json(docs))
         .catch((err) => {
             console.error(err);
             res.status(500);
             res.json({status:500, error:err});
-        })
+        });
+    });
+
+    router.post('/', (req, res) => {
+        //can we add a podcast in our app?
+        //do we use this post to add to our subscribed list?
     })
+
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id //id of database item;
+        collection.deleteOne({_id: ObjectID(id)})
+        .then((doc) => res.json(doc))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({status: 500, error:err});
+        })
+        
+    })
+
+    //do we need an update route? can we update podcasts?
 
     return router
 }
+
+module.exports = createRouter
