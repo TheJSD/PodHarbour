208f58275d2f64656ad474450e6f963347e07c85
James Dickson <jamesstuartdickson@gmail.com>
2023-09-05 15:26:24 +0100

    routes started - shows home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,11 @@ import './App.css'
 import Hero from './assets/Hero.png'
 import GridContainer from './containers/GridContainer';
 import Placeholder from './assets/150.png'
+import { Routes, Route, Outlet, Link } from 'react-router-dom'
+import HomeContainer from './containers/HomeContainer';
 
 function App() {
 
-const [caroArray, setCaroArray] = useState(0);
 const [podcasts, setPodcasts] = useState([])
 const [loading, setLoading] = useState(true);
 
@@ -19,60 +20,43 @@ useEffect(() => {
 const fetchPodcasts = () => {
   fetch(podcastsURL)
   .then(response => response.json())
-  .then(data => setPodcasts(data)); setLoading(false);
+  .then(data => setPodcasts(data))
+  .then(() => setLoading(false));
 }  
 
-const nextCaro = (e) => {
-  setCaroArray(caroArray +5)
-}
-
-const prevCaro = (e) => {
-  setCaroArray(caroArray -5)
-}
-
-
-const Carousel = () => { 
-  return(
-  <>
-  <div className="podcast-list">
-  <ul>
-  <button onClick={prevCaro}className="button-caro">-</button>
-  {podcasts.map((podcast, index) => <li className="podcast"
-  ><div className="podcast-box">
-  <img className="podcast-img" src={Placeholder}></img><br />
-  <b>{podcast.name}</b><br />
-  <i>{podcast.author}</i></div><br /></li>)}
-  <button onClick={nextCaro}className="button-caro">+</button>
-  </ul>
-  </div>
-  </>)
-  }
 
   return (
-  <>
-  <nav className="nav">
-    <button className="button-small">All Podcasts</button><br />
-    <button className="button-small">Saved Podcasts</button><br />
-    <button className="button-small">Help</button><br />
-  </nav>
+    <Routes>
+      <Route path="/">
+        <Route index element={<HomeContainer podcasts={podcasts}/>}/>
+      </Route>
 
-  <div className="backround">
-    <div className="hero-container">
-      <div className="hero">
-      <img className="hero-picture" src={Hero}></img>
-      <h2>Welcome to PodHarbour!</h2>
-      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-      <div className="button-outer">
-      <button className="button-small-hero">Test Button</button><button className="button-small-hero">Test Button</button>
-      </div>
-      </div>
-      </div>
-    <Carousel />
-  </div>
-  <footer>
-  {caroArray}
-  </footer>
-  </>
+    </Routes>
+
+  // <>
+  // <nav className="nav">
+  //   <button className="button-small">All Podcasts</button><br />
+  //   <button className="button-small">Saved Podcasts</button><br />
+  //   <button className="button-small">Help</button><br />
+  // </nav>
+
+  // <div className="backround">
+  //   <div className="hero-container">
+  //     <div className="hero">
+  //     <img className="hero-picture" src={Hero}></img>
+  //     <h2>Welcome to PodHarbour!</h2>
+  //     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+  //     <div className="button-outer">
+  //     <button className="button-small-hero">Test Button</button><button className="button-small-hero">Test Button</button>
+  //     </div>
+  //     </div>
+  //     </div>
+  //   <Carousel />
+  // </div>
+  // <footer>
+  // {caroArray}
+  // </footer>
+  // </>
   )
 }
 
---
+++ client/src/containers/HomeContainer.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { useState } from 'react'
+import Hero from '../assets/Hero.png'
+import Placeholder from '../assets/150.png'
+
+const HomeContainer = ({podcasts}) => {
+
+  const [caroArray, setCaroArray] = useState(0);
+
+  const nextCaro = (e) => {
+  setCaroArray(caroArray +5)
+}
+
+const prevCaro = (e) => {
+  setCaroArray(caroArray -5)
+}
+
+const Carousel = () => { 
+  return(
+  <>
+  <div className="podcast-list">
+  <ul>
+  <button onClick={prevCaro}className="button-caro">-</button>
+  {podcasts.map((podcast, index) => <li className="podcast"
+  ><div className="podcast-box">
+  <img className="podcast-img" src={Placeholder}></img><br />
+  <b>{podcast.name}</b><br />
+  <i>{podcast.author}</i></div><br /></li>)}
+  <button onClick={nextCaro}className="button-caro">+</button>
+  </ul>
+  </div>
+  </>)
+  }
+
+  return (
+    <>
+    <nav className="nav">
+      <button className="button-small">All Podcasts</button><br />
+      <button className="button-small">Saved Podcasts</button><br />
+      <button className="button-small">Help</button><br />
+    </nav>
+  
+    <div className="backround">
+      <div className="hero-container">
+        <div className="hero">
+        <img className="hero-picture" src={Hero}></img>
+        <h2>Welcome to PodHarbour!</h2>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        <div className="button-outer">
+        <button className="button-small-hero">Test Button</button><button className="button-small-hero">Test Button</button>
+        </div>
+        </div>
+        </div>
+      <Carousel />
+    </div>
+    <footer>
+    {caroArray}
+    </footer>
+    </>
+  )
+}
+
+export default HomeContainer
