b09333a6bb3b32a8126c55a28137847e9505ef5c
James Dickson <jamesstuartdickson@gmail.com>
2023-09-05 11:38:39 +0100

    fixed router, calls to api should be working

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
-const express= require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const MongoClient = require('mongodb').MongoClient
-const createRouter = require('')
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology: true})
 .then((client) => {
     const db = client.db('podharbour');
-    const podcastsCollection = db.collection('podcasts')
-    const usersCollection = db.collection('users')
-
-    //Create router for podcasts and users
-    // app.use('/api/podcasts')
-})
-.catch(console.err)
+    const podcastsCollection = db.collection('podcasts');
+    const usersCollection = db.collection('users');
+    const podcastsRouter = createRouter(podcastsCollection);
+    const usersRouter = createRouter(usersCollection);
+    app.use('/api/podcasts', podcastsRouter);
+    app.use('/api/users', usersRouter);
+    })
+.catch(console.err);
 
 app.listen(9000, function (){
     console.log(`Listening on port ${this.address().port}`);
