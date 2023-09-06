fcd79fb95a393ac9dcb9f8a7e5203393788f41f4
Lewis Ferguson <lewis.h.ferguson@btinternet.com>
2023-09-06 11:47:23 +0100

    added route for all podcasts page, small changes to nav bar, added css to all podcasts page

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,36 +2,50 @@ import React from 'react'
 import styled from 'styled-components'
 
 
+const NavLink = styled.a`
+  text-decoration: none;
+  color:inherit;
+    &&:hover {
+      text-decoration: none;
+      color:inherit;
+    }
+    &&:active {
+      text-decoration: none;
+    color:inherit;
+    }
+    &&:hover {
+      text-decoration: none;
+      color:inherit;
+    }
+
+`
+const NavButton = styled.button`
+background: transparent;
+padding: 15px;
+color: #32334D;
+border: none;
+font-size: 16px;
+font-weight:bold;
+text-align:left;
+width:140px;
+height: 60px;
+&:hover {
+    background-color: #ffb83497;
+    transition: 0.8s;
+    border-radius: 5px;
+    color: #32334D;
+}
+`
 const NavBar = () => {
 
-    const NavButton = styled.button`
-        background: transparent;
-        padding: 15px;
-        color: #32334D;
-        border: none;
-        font-size: 16px;
-        font-weight:bold;
-        text-align:left;
-        width:175px;
-        height: 60px;
-        &:hover {
-            background-color: #ffb83497;
-            transition: 0.8s;
-            border-radius: 5px;
-            color: #32334D;
-        }
-    `
-
   return (
-
     <nav className="nav">
-        <NavButton>Home</NavButton><br />
-        <NavButton>All Podcasts</NavButton><br />
+        <NavButton><NavLink href="/">Home</NavLink></NavButton><br />
+        <NavButton><NavLink href="/all">All Podcasts</NavLink></NavButton><br />
         <NavButton>Saved Podcasts</NavButton><br />
         <NavButton>Help</NavButton><br />
         <NavButton>About Us</NavButton><br />
     </nav>
-
   )
 }
 
