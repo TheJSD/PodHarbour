fd3093c7a253826acc0afe99e8ccac48ecbe122c
Holly Tyler <hollystertyler@gmail.com>
2023-09-10 13:41:37 +0100

    Seed nearly finished

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -93,25 +93,25 @@ db.podcasts.insertMany([
     {
         name: "CrunchCast",
         author: "Kirsty Crunch",
-        description: "Listen in as we crunch on some foods",
+        description: "I love crunchy foods, do you? Even if you don't, maybe you love listening to other people crunch on them. I think we can all relate to that. If that sounds like your cup of tea then you are in the right place my friend! Join me on this ASMR journey through this world of crunch, pop and slurps.",
         genre: "Food",
         image: 'CrunchCast',
         episodes: [
             {
                 title: 'Crisps',
-                description: 'On this episode we crunch on a packet of crisps',
+                description: 'In todays episode I will will be opening a bag of crisps slowly and crunch away on each and every single one of them. Join me for tingly crunchy feelings.',
                 image: 'CrunchCast',
                 audio: ''
             },
             {
                 title: 'Biscuits',
-                description: "On this episode we crunch on a packet of biscuits",
+                description: "On this episode we crunch on a packet of biscuits. Mmmmmm I love biscuits. They are very british aren't they? I found out that most Americans don't even know what our version of biscuits is. To them biscuits are fluffy and soft, to use they are crunchy and delicious. Join me for a lovely day of crunch biscuits and crinkly packets.",
                 image: 'CrunchCast',
                 audio: ''
             },
             {
                 title: 'Hot Chocolate',
-                description: 'As a twist, on this episode we sip on some hot chocolate',
+                description: "As a twist, on this episode we sip on some hot chocolate. I told you we would be hearing some slurping didnt I? Pour yourself a cup of tea, coffee, or hot cocoa and join me for a slurpy cozy day. Im sure there will be plenty of whispers in your ear as well;)",
                 image: 'CrunchCast',
                 audio: ''
             }
@@ -119,27 +119,27 @@ db.podcasts.insertMany([
         
     },
     {
-        name: "CrunchCast2",
-        author: "Kirsty Crunch",
-        description: "Listen in as we crunch on some foods",
-        genre: "Food",
+        name: "Just the Two of Us",
+        author: "John and Joe Watson",
+        description: "Hi, I'm John, and hes Joe, and together we are, TWINCAST! Born only moments apart from eachother, Joe and I have always been close. In this podcast we will share our experience of life as identical twins! We've had ups and downs over the past 28 years, and we want to share them all with you! Join us on our journey together, through life as Twins!",
+        genre: "Life",
         image: 'CrunchCast',
         episodes: [
             {
-                title: 'Crisps',
-                description: 'On this episode we crunch on a packet of crisps',
+                title: 'Look Whos Talkin - Episode 1',
+                description: "Okay let's start this pod off right! Today we are going to play a little games, it's called Looks Who's Talkin'! John and I will take turns reading out from a script, but it won't be in order. Its your job as the audience to chime in and try to guess whos talkin! We will award points to those of you who guess right!",
                 image: 'CrunchCast',
                 audio: ''
             },
             {
-                title: 'Biscuits',
-                description: "On this episode we crunch on a packet of biscuits",
+                title: 'What Happened to Us? - Episode 2',
+                description: "Alright, in todays episode lets share some stories. Joe and I have gotten into plenty of mischief together (and separately) whilst growing up. Today we want to share with you some of our most embarrassing and hilarious childhood stories. Join us and enjoy a day of laughs and smiles as we take a trip down memory lane and giggle our way through each story.",
                 image: 'CrunchCast',
                 audio: ''
             },
             {
-                title: 'Hot Chocolate',
-                description: 'As a twist, on this episode we sip on some hot chocolate',
+                title: 'Trading Places - Episode 3',
+                description: "Have you ever wondered what it would be like to have a twin and attempt to trade places them? Well guess what, we did that once, for a WHOLE WEEK! This story is a long one, and a good one, so buckle up for this long episode where we tell you all about the time where John became Joe and Joe became John for and entire week. We managed to deceive our friends, our family, our teachers, and even our girlfriends!",
                 image: 'CrunchCast',
                 audio: ''
             }
@@ -147,27 +147,33 @@ db.podcasts.insertMany([
         
     },
     {
-        name: "CrunchCast3",
-        author: "Kirsty Crunch",
-        description: "Listen in as we crunch on some foods",
-        genre: "Food",
+        name: "Nightfall Mysteries",
+        author: "Spooky Steve",
+        description: "*BUMP* We all hear those bumps in the night and wonder if its just the house creaking, the wind blowing, or something far more sinister. Unfortunately for some very unlucky folks on this earth that bump ends up being something very sinister. My job is to dig into the gruesome details of true crime, mysteries and scary ghost tails, your job is to dive deeper with me and enjoy every shiver thats sent down your spine.",
+        genre: "Horror, True Crime",
         image: 'CrunchCast',
         episodes: [
             {
-                title: 'Crisps',
-                description: 'On this episode we crunch on a packet of crisps',
+                title: 'The Gruesome Kidnapping of Kathy K. - Episode 1',
+                description: "Kathy was a 26 year old University student in South West UK. Her friends family and peers describe her as a shining star ready to start her life and career as a news journalist. But unfortunately Kathys shot at that beautiful life and future was cut short because one vile person on this planet couldnt stand to just sit back and watch her succeed. Join me today as we look into Kathys tragic short life and uncover what happened to her together.",
                 image: 'CrunchCast',
                 audio: ''
             },
             {
-                title: 'Biscuits',
-                description: "On this episode we crunch on a packet of biscuits",
+                title: 'Where is Sam Peterson - Episode 2',
+                description: "In the quiet evening of July 13, 2003 in small town rural Jamesville Ohio, all seemed peaceful and calm. The birds were sleeping and the crickets were alive and as the fog rested its cold touch upon the land one cyclist was riding down interstate 56 when they suddenly seemingly disappeared off the face of this earth. The cyclist in question was Angel Gonzalez, a local resident that was on his was home from a long night at work. Police have been searching for Angel for over ten years now with little to no leads to work from. Join me as we try to crack this cold case together and discover what truly happened to Angel on that cool summer night.",
                 image: 'CrunchCast',
                 audio: ''
             },
             {
-                title: 'Hot Chocolate',
-                description: 'As a twist, on this episode we sip on some hot chocolate',
+                title: 'Ghosts - Episode 3',
+                description: "Ghosts, do you believe in them? What do you think they are if you do? There are so mayn theories out there about what ghosts are, whether or not they are real. Are they demons, are they relatives taht have passed on, are they creatures from another dimension? Lets dive into a day of looking into the best evidence possible to represent our ideas about what ghosts are and where they come from.",
+                image: 'CrunchCast',
+                audio: ''
+            },
+            {
+                title: 'Ghosts continued- Episode 4',
+                description: "Last week we started exploring and discussing our ideas of what ghosts are and whether or not they are real, this week after looking into a bunch of evidence we will be diving even deeper into more evidence and talking baout what we see and what it means.",
                 image: 'CrunchCast',
                 audio: ''
             }
@@ -175,90 +181,28 @@ db.podcasts.insertMany([
         
     },
     {
-        name: "CrunchCast4",
-        author: "Kirsty Crunch",
-        description: "Listen in as we crunch on some foods",
-        genre: "Food",
+        name: "We're Not Even That Funny",
+        author: "The Humour Squad",
+        description: "Five People, one desk, five microphones, too many boring jokes, too many embarassing stories. Join us and maybe you will laugh.",
+        genre: "Comedy, Real Life",
         image: 'CrunchCast',
         episodes: [
             {
-                title: 'Crisps',
-                description: 'On this episode we crunch on a packet of crisps',
+                title: 'Episode 1: Dad Jokes',
+                description: "Once I was kidnapped but a mime, they did unspeakable things to me. Did you just laugh? Good, then youll love todays episode where we tell you dad jokes and you laugh at them",
                 image: 'CrunchCast',
                 audio: ''
             },
             {
-                title: 'Biscuits',
-                description: "On this episode we crunch on a packet of biscuits",
-                image: 'CrunchCast',
-                audio: ''
-            },
-            {
-                title: 'Hot Chocolate',
-                description: 'As a twist, on this episode we sip on some hot chocolate',
+                title: 'Episode 2: Fart Jokes',
+                description: "Everyone farts, and sometimes its funny and comes with a story that sbeen told about 50 times over. Today we will share our stories about all the times that we managed to clear a room or offend our grandmas by farting at the dinner table. Join us and maybe you will laugh.",
                 image: 'CrunchCast',
                 audio: ''
             }
         ]
         
-    },
-    {
-        name: "CrunchCast5",
-        author: "Kirsty Crunch",
-        description: "Listen in as we crunch on some foods",
-        genre: "Food",
-        image: 'CrunchCast',
-        episodes: [
-            {
-                title: 'Crisps',
-                description: 'On this episode we crunch on a packet of crisps',
-                image: 'CrunchCast',
-                audio: ''
-            },
-            {
-                title: 'Biscuits',
-                description: "On this episode we crunch on a packet of biscuits",
-                image: 'CrunchCast',
-                audio: ''
-            },
-            {
-                title: 'Hot Chocolate',
-                description: 'As a twist, on this episode we sip on some hot chocolate',
-                image: 'CrunchCast',
-                audio: ''
-            }
-        ]
-        
-    },
-    {
-        name: "CrunchCast6",
-        author: "Kirsty Crunch",
-        description: "Listen in as we crunch on some foods",
-        genre: "Food",
-        image: 'CrunchCast',
-        episodes: [
-            {
-                title: 'Crisps',
-                description: 'On this episode we crunch on a packet of crisps',
-                image: 'CrunchCast',
-                audio: ''
-            },
-            {
-                title: 'Biscuits',
-                description: "On this episode we crunch on a packet of biscuits",
-                image: 'CrunchCast',
-                audio: ''
-            },
-            {
-                title: 'Hot Chocolate',
-                description: 'As a twist, on this episode we sip on some hot chocolate',
-                image: 'CrunchCast',
-                audio: ''
-            }
-        ]
-        
-    },
-])
+    }
+]);
 
 db.users.insertOne({
     name:"user",
